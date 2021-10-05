7d3d35abdb46c7beab6edb3ccf04819fdf9594cb
felipedeaquino <felipe.puziol@gmail.com>
2021-10-04 23:27:38 -0300

    day #8.1 (readability update)

--- replit-boilerplate-express/myApp.js
+++ replit-boilerplate-express/myApp.js
@@ -3,10 +3,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var app = express();
 
-var connect = mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//middleware logger of requests prints server requests
-app.get('/json', function(req, res, next) {
+//middleware logger of requests; prints server requests
+app.get('/json', (req, res, next) => {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
@@ -19,13 +17,13 @@ app.use("/public", express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
 //serves html index file
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
   //res.send(console.log(__dirname))
 });
 
 //apply some logic using secrets (environment variables) also serving JSON to a certain route
-app.get('/json', function(req, res) {
+app.get('/json', (req, res) => {
   var text = "Hello json";
   if (process.env.MESSAGE_STYLE == "uppercase") {
     text = text.toUpperCase();
@@ -37,7 +35,7 @@ app.get('/json', function(req, res) {
 }); 
 
 //time server using "chain middleware" logic
-app.get('/now', function(req, res, next) {
+app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
 }, function(req, res) {
@@ -45,13 +43,13 @@ app.get('/now', function(req, res, next) {
 });
 
 //gets route parameter from client
-app.get("/:word/echo", function(req, res) {
+app.get("/:word/echo", (req, res) => {
   var word = req.params.word;
   res.json({"echo" : word});
 });
 
 //gets query parameter from client
-app.get("/name", function(req, res) {
+app.get("/name", (req, res) => {
   res.json({ name: req.query.first + " " + req.query.last});
 });
 
@@ -63,7 +61,7 @@ app.get("/name", function(req, res) {
 });*/
 
 //gets data from post requests
-app.post("/name", function(req, res) {
+app.post("/name", (req, res) => {
   res.json({ name: req.body.first + " " + req.body.last});
 });
 
