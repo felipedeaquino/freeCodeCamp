d6cecd8a4e39a1cfd2fd357b8ab6fbaf92efea4a
felipedeaquino <felipe.puziol@gmail.com>
2022-07-25 01:03:23 -0300

    finished prototype

--- file-metadata/src/server.js
+++ file-metadata/src/server.js
@@ -1,8 +1,18 @@
-import dotenv from "dotenv";
-import express from "express";
+import dotenv from 'dotenv';
+import express from 'express';
+import { routes } from './routes.js';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
+
+app
+  .use(routes)
+  .use(express.static(path.join(__dirname, path.sep, '..', path.sep, 'public')));
 
 const port = process.env["PORT"] || 3000;
 
